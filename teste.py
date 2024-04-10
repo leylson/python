63f6ac109c0caee8ab1aa5df26a524cecb991df8
Leylson Rodrigues <leylson85rodrigues@gmail.com>
2024-04-10 11:45:48 -0300

    Python teste

---
+++ teste.py
@@ -0,0 +1,17 @@
+# Importa biblioteca
+import pyautogui
+
+#https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Action click in win
+pyautogui.press("win")
+#escreva
+pyautogui.write("Winthor")
+#aperte
+pyautogui.press("enter")
+
+#apertar atalho
+# pyautogui.hotkey("ctrl", "C")
+# click
+#pyautogui.click()
+# write -> escrever  
+#pyautogui.write("")
